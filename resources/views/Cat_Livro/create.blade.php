f613dde3f1729b0739d13bea23a8271b77110f2f
Andre <andre.5470798@aluno.mg.gov.br>
2022-10-18 19:33:44 -0300

    Retirado a pasta Cat_Autor e transferido o create.blade.php do mesmo para Cat_Livro

---
+++ resources/views/Cat_Livro/create.blade.php
@@ -0,0 +1,48 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Categoria</h1>
+@stop
+@section('content')
+@if($errors->any())
+<div class="alert alert-danger">
+    <ul>
+        <h4>
+            <i class="icon fas fa-ban"></i>ocorreu o seguinte error_clear_last 
+            @foreach($errors->all() as $erro)
+            <li>
+                <h5>
+                    {{$erro}}
+                </h5>
+            </li>
+            @endforeach
+
+        </h4>
+    </ul>
+</div>
+@endif
+<div class="card card-primary">
+<div class="card-header">
+<h3 class="card-title">Cadastro de Categorias </h3>
+</div>
+
+
+<form method="POST" action="{{route('salvar')}}">
+@csrf
+<div class="card-body">
+<div class="form-group">
+<label for="exampleInputEmail1">Categoria</label>
+<input type="text" class="form-control" id="exampleInputEmail1" placeholder="Categoria do Autor Ex:romance, suspense, ficção..." name="descr_categ">
+</div>
+
+
+<div class="card-footer">
+<button type="submit" class="btn btn-success">Cadastrar</button>
+<button type="reset" class="btn btn-danger">Cancelar</button>
+</div>
+</form>
+</div>
+
+@endsection
