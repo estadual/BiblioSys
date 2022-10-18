db54d9e7a5f97c509741dc78958e65bd526155e2
Andre <andre.5470798@aluno.mg.gov.br>
2022-10-17 22:02:01 -0300

    Commit feito dos arquivos da maquina do Andre

---
+++ app/Http/Controllers/CategoriaController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Categoria;
+
+
+class CategoriaController extends Controller
+{
+
+    public function cadastrar(){
+        return view('categoria.create');
+    }
+
+    public function store(Request $request){
+        $data = $request->only([
+            'descr_categ'
+        ]);
+       
+        $validator = Validator::make($data,[
+            'descr_categ'=>['required','string','unique:categoria_livro']
+        ]);
+        if($validator->fails()){
+            return redirect()->route('cadastrar')->withErrors($validator)->withInput();
+        }
+        $categoria = new Categoria;
+        $categoria->descr_categ = $data ['descr_categ'];
+        $categoria->save();
+         return redirect()->route('cadastrar');
+        }
+
+    
+}
